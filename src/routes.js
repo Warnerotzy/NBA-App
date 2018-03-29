6807b7734d5ee8e9cb4510026a97f7bea25686aa
Warnerotzy <adrianmartinez.sopra@gmail.com>
2018-03-29 11:45:26 +0200

    Video Article + Video List

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Layout from './hoc/Layout/Layout';
 import NewsArticle from './components/Articles/News/Post/index';
+import VideoArticle from './components/Articles/Videos/Video/index';
 
 
 class Routes extends Component {
@@ -12,6 +13,7 @@ class Routes extends Component {
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/articles/:id" exact component={NewsArticle} />
+                    <Route path="/videos/:id" exact component={VideoArticle} />
                 </Switch>
             </Layout>
         );
