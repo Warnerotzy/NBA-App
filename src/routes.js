e242c1ebb02f8d8d1ac7b04092bc866253c36670
Warnerotzy <adrianmartinez.sopra@gmail.com>
2018-03-29 12:39:24 +0200

    Alpha release

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Layout from './hoc/Layout/Layout';
 import NewsArticle from './components/Articles/News/Post/index';
 import VideoArticle from './components/Articles/Videos/Video/index';
 import NewsMain from './components/Articles/News/Main/index';
+import VideosMain from './components/Articles/Videos/Main';
 
 class Routes extends Component {
     render() {
@@ -13,6 +14,7 @@ class Routes extends Component {
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/news" exact component={NewsMain} />
+                    <Route path="/videos" exact component={VideosMain} />
                     <Route path="/articles/:id" exact component={NewsArticle} />
                     <Route path="/videos/:id" exact component={VideoArticle} />
                 </Switch>
