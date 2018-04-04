f8d217392cad121a5f4e10f355b9f73d84a4dddc
Warnerotzy <adrianmartinez.sopra@gmail.com>
2018-04-04 12:03:04 +0200

    Fixed Articles post & Videos posts

--- src/components/Articles/News/Post/index.js
+++ src/components/Articles/News/Post/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { URL } from '../../../../config';
+import { firebaseDB, firebaseLooper, firebaseTeams } from '../../../../firebase';
+
 import styles from '../../articles.css';
 import Header from './header';
 
@@ -12,7 +12,21 @@ class NewsArticles extends Component {
     }
 
     componentWillMount() {
-        axios.get(`${URL}/articles?id=${this.props.match.params.id}`)
+
+        firebaseDB.ref(`articles/${this.props.match.params.id}`).once('value')
+        .then((snapshot)=>{
+            let article = snapshot.val();
+            firebaseTeams.orderByChild('id').equalTo(article.team).once('value')
+            .then((snapshot)=>{
+                const team = firebaseLooper(snapshot);
+                this.setState({
+                    article,
+                    team
+                })
+            })
+        });
+
+        /* axios.get(`${URL}/articles?id=${this.props.match.params.id}`)
         .then( response => {
             let article = response.data[0];
             axios.get(`${URL}/teams/?id=${article.team}`)
@@ -22,7 +36,7 @@ class NewsArticles extends Component {
                     team: response.data
                 })
             })
-        })
+        }) */
     }
 
     render() {
--- src/components/Articles/Videos/Video/index.js
+++ src/components/Articles/Videos/Video/index.js
@@ -4,6 +4,8 @@ import { URL} from '../../../../config';
 import styles from '../../articles.css';
 import Header from './header';
 import VideosRelated from '../../../widgets/VideosList/VideosRelated/videosRelated';
+import { firebaseDB, firebaseLooper, firebaseTeams, firebaseVideos } from '../../../../firebase';
+
 
 class VideoArticle extends Component {
     state = {
@@ -14,7 +16,24 @@ class VideoArticle extends Component {
     }
 
     componentWillMount() {
-        axios.get(`${URL}/videos?id=${this.props.match.params.id}`)
+
+        firebaseDB.ref(`videos/${this.props.match.params.id}`).once('value')
+            .then((snapshot) => {
+                let article = snapshot.val();
+                firebaseTeams.orderByChild('teamId')
+                .equalTo(article.team)
+                .once('value')
+                .then((snapshot) => {
+                        const team = firebaseLooper(snapshot);
+                        this.setState({
+                            article,
+                            team
+                        })
+                        this.getReleated();
+                    })
+            });
+
+        /* axios.get(`${URL}/videos?id=${this.props.match.params.id}`)
             .then(response => {
                 let article = response.data[0];
                 axios.get(`${URL}/teams/?id=${article.team}`)
@@ -25,11 +44,28 @@ class VideoArticle extends Component {
                         });
                         this.getReleated();
                     })
-            })
+            }) */
     }
 
     getReleated = () => {
-        axios.get(`${URL}/teams`)
+        firebaseTeams.once('value')
+        .then((snapshot)=> {
+            const teams = firebaseLooper(snapshot);
+
+            firebaseVideos.orderByChild('team')
+            .equalTo(this.state.article.team)
+            .limitToFirst(3).once('value')
+            .then((snapshot)=>{
+                const related = firebaseLooper(snapshot);
+                this.setState({
+                    teams,
+                    related
+                })
+            })
+
+        })
+
+        /* axios.get(`${URL}/teams`)
         .then( response => {
             let teams = response.data;
             axios.get(`${URL}/videos?q=${this.state.team[0].city}&_limit=3`)
@@ -39,7 +75,7 @@ class VideoArticle extends Component {
                     related: response.data
                 })
             })
-        })
+        }) */
     }
 
     render() {
