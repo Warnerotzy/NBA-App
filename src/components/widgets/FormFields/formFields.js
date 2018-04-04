f22c8cee9b258a664ed18f57e56a35d513d91c73
Warnerotzy <adrianmartinez.sopra@gmail.com>
2018-04-04 12:59:53 +0200

    Form Fields validation

--- src/components/widgets/FormFields/formFields.js
+++ src/components/widgets/FormFields/formFields.js
@@ -2,6 +2,20 @@ import React from 'react';
 import styles from './formFields.css';
 
 const FormFields = ({formdata, change, id}) => {
+
+    const showError = () =>{
+        let errorMessage = null;
+
+        if(formdata.validation && !formdata.valid){
+            errorMessage = (
+                <div className={styles.labelError}>
+                    {formdata.validationMessage}
+                </div>
+            )
+        }
+
+        return errorMessage;
+    }
     const renderTemplate = () =>{
         let formTemplate = null;
 
@@ -15,6 +29,7 @@ const FormFields = ({formdata, change, id}) => {
                             onBlur={(event) => change({event, id, blur:true})}
                             onChange={(event) => change({event, id, blur:false})}
                         />
+                        {showError()}
                     </div>
                 )
                 break;
